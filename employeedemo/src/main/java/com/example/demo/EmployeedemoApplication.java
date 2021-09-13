e3af38175a75b7113452b662fdae24f88c3b6ccc
ajitha_nalluri <ajitha.nalluri@otsi-usa.com>
2021-09-13 18:27:13 +0530

    code with dockerfile

---
+++ employeedemo/src/main/java/com/example/demo/EmployeedemoApplication.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class EmployeedemoApplication {
+
+//	   @Value("${message}")
+//	   static String message;
+//	   
+	public static void main(String[] args) {
+		
+		SpringApplication.run(EmployeedemoApplication.class, args);
+	}
+	@GetMapping("/getip")
+	public String getIp() throws UnknownHostException
+	{
+		InetAddress ip = InetAddress.getLocalHost();
+		//log.info(" From host: " + ip);
+		System.out.println(" From host: " + ip);
+	    return " From host: " + ip;
+	}
+
+}
